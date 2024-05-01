8dc6aaa153941ccb640119907b0e23b47b7ec33a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-01 20:27:16 -0300

    Exemplo de condição simples

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 78.2;
+console.log(`A velocidade do seu carro é ${velocidade}km/h`);
+
+if (velocidade > 60) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
+
+console.log("Dirija sempre usando cinto de segurança!");
