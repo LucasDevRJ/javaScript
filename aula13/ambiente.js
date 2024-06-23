dc6c0e9f3e2563ebd3fd63da3ef2dbe9090c65e6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-06-23 13:18:01 -0300

    Exemplo de estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var contador = 1;
+do {    
+    console.log("Tudo bem");
+    contador++;
+} while (contador <= 6);
+
+/*
+var contador = 1;
+while (contador <= 6) {
+    console.log("Tudo bem");
+    contador++;
+}
+*/
+
+/*
+console.log("Tudo bem");
+console.log("Tudo bem");
+console.log("Tudo bem");
+console.log("Tudo bem");
+console.log("Tudo bem");
+console.log("Tudo bem");
+*/
