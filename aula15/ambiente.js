860ecdaabd26292e1cb68e6d06339e1473ed910b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-07-14 19:54:39 -0300

    Testando métodos do vetor

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,2 +1,10 @@
 let num = [5, 8, 2, 9, 3];
 console.log(`Nosso vetor é o ${num}`);
+console.log(`Nosso vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do nosso vetor é ${num[0]}`);
+
+num.sort(); //embaralha o vetor
+console.log(`Nosso vetor é o ${num}`);
+
+num.push(1); //adiciona valor no final
+console.log(`Nosso vetor é o ${num}`);
